8edca9c75b2db9153b5ba8ddb90ea7f650ed5c67
ks6088ts <ks6088ts@gmail.com>
2026-02-04 12:13:29 +0900

    update versions

--- infra/modules/azure/linux_vm/versions.tf
+++ infra/modules/azure/linux_vm/versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 4.0.0"
+      version = ">= 4.58.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = ">= 4.0.0"
+      version = ">= 4.2.1"
     }
   }
 }
