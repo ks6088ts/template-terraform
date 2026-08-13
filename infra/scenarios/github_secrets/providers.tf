01aec548098e9699c624e74b820379b6658b6288
ks6088ts <ks6088ts@gmail.com>
2026-08-13 11:11:29 +0900

    refactor(terraform): remove unused azurerm provider from GitHub secrets scenario

--- infra/scenarios/github_secrets/providers.tf
+++ infra/scenarios/github_secrets/providers.tf
@@ -1,8 +1,3 @@
 provider "github" {
   owner = var.github_owner
 }
-
-# NOTE: When using the backend azurerm, the azurerm provider must be declared even if not used directly.
-provider "azurerm" {
-  features {}
-}
